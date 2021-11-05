2620b1eca6bbb7402ca218b0367efcdfeca8e320
Kirill <agoravobia@gmail.com>
2021-11-05 13:14:09 +0200

    add object personalMovieDB and her propertys in main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 let numberOfList = prompt("Сколько фильмов вы посмотрели?:");
+
+let personalMovieDB = new Object();
+personalMovieDB.count = numberOfList;
+personalMovieDB.movies = new Object();
+personalMovieDB.actors = new Object();
+personalMovieDB.genres = new Array();
+personalMovieDB.privat = false;
+
